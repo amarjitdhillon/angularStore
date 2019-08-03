82d34fb1a6c29a99a23848639b6d46dce4820a17
odd.snail <amarjitdhillon@cmail.carleton.ca>
2019-08-03 02:30:17 -0400

    added the home and the about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpCreateComponent } from './emp-create/emp-create.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpCreateComponent
+    EmpCreateComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
